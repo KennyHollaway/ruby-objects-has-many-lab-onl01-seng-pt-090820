365af72fca0491a5f288b2bfa1e1c298e1096d4a
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-02 00:18:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,10 +12,9 @@ class Author
   end
  
   def add_post(post)
-    
+    @@post_count += 1
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
